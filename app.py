b574c495909cfbe0d7cb20fc5e18b9478de6d052
Josh Li <zilhsoj@gmail.com>
2019-10-09 15:57:20 -0400

    implement endpoint

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 # classes in pYthon is capitalized, library is lowercase for all
 
-# first thing is to create a instance of Flask class, __name__ is to give each fiel a unique name
+# first thing is to create a instance of Flask class, __name__ is to give each file a unique name
 app = Flask(__name__)
 
 # first to tell our app what request to understand
@@ -11,6 +11,29 @@ app = Flask(__name__)
 def home():
     # It can do things in this method, but whatever it does, it has to return something back to the browser
     return 'Hello world!'
+
+@app.route('/store', methods=['POST'])
+def create_store():
+    request_data = request.get_json()
+    new_store = {
+            'name': request_date['name']
+            'items': []
+    }
+    stores.append(new_store)
+    return jsonify(new_store)
+
+@app.route('/store/<string:name>')
+def get_store(name)
+    # iterate over stores
+    # if match, return the store
+    # if no match, return an error message
+
+    for store in store:
+        if store['name'] == name:
+            return jsonify(store)
+        else:
+            return jsonify({'message':'store not found'})
+
 
 # Also need to tell the app to start running
 # specify a port in run()
